7e45a6846326908245de504783783ae2cee8876c
Shadowrabbit <yingtu0401@gmail.com>
2022-10-22 15:00:38 +0800

    v1.4 support

---
+++ 1.4/Source/ModRimWorldNoDecreeQuest/Patch/PatchMain.cs
@@ -0,0 +1,26 @@
+﻿// ******************************************************************
+//       /\ /|       @file       PatchMain.cs
+//       \ V/        @brief      
+//       | "")       @author     Shadowrabbit, yingtu0401@gmail.com
+//       /  |                    
+//      /  \\        @Modified   2021-06-08 13:21:20
+//    *(__\_\        @Copyright  Copyright (c) 2021, Shadowrabbit
+// ******************************************************************
+using System.Reflection;
+using HarmonyLib;
+using JetBrains.Annotations;
+using Verse;
+
+namespace RabitSquare.NoDecreeQuest
+{
+	[UsedImplicitly]
+	[StaticConstructorOnStartup]
+	public class PatchMain
+	{
+		static PatchMain()
+		{
+			var instance = new Harmony("RabiSquare.NoDecreeQuest");
+			instance.PatchAll(Assembly.GetExecutingAssembly());
+		}
+	}
+}
